272131ea50e6f14db51f02e94db4f96067c3c0c7
and <surftware@gmail.com>
2020-07-24 17:51:22 -0500

    adición favicon

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,14 +1,23 @@
             <meta charset="utf-8">
             <meta name="apple-mobile-web-app-capable" content="yes" />
             <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
-            <meta name="format-detection" content="telephone=no" /> 
-            <link rel="shortcut icon" href="favicon.ico"/> 
-            <link rel="stylesheet" type="text/css" href="css/fontello.css"/>
+            <meta name="format-detection" content="telephone=no" />
+            <link rel="shortcut icon" href="favicon.ico" />
+            <link rel="stylesheet" type="text/css" href="css/fontello.css" />
             <link rel="stylesheet" type="text/css" href="css/animate.css" />
-            <link rel="stylesheet" type="text/css" href="css/idangerous.swiper.css"/>   
-            <link rel="stylesheet" type="text/css" href="css/style.css"/>
-            <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/> <!--boostrap 3.3.6 --> 
-            <link href="fontawesome/css/all.css" rel="stylesheet"> <!--load all styles -->
-                
-            
-             
+            <link rel="stylesheet" type="text/css" href="css/idangerous.swiper.css" />
+            <link rel="stylesheet" type="text/css" href="css/style.css" />
+            <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+            <!--boostrap 3.3.6 -->
+            <link href="fontawesome/css/all.css" rel="stylesheet">
+            <!--load all styles -->
+
+
+
+            <link rel="apple-touch-icon" sizes="120x120" href="assets/apple-touch-icon.png">
+            <link rel="icon" type="image/png" sizes="32x32" href="assets/favicon-32x32.png">
+            <link rel="icon" type="image/png" sizes="16x16" href="assets/favicon-16x16.png">
+            <link rel="manifest" href="assets/site.webmanifest">
+            <link rel="mask-icon" href="assets/safari-pinned-tab.svg" color="#426c6e">
+            <meta name="msapplication-TileColor" content="#ef5b5b">
+            <meta name="theme-color" content="#ef5b5b">

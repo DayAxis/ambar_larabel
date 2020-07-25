d26b85be59c8e14f5b08ee788e9b02e8252a5fca
and <surftware@gmail.com>
2020-07-25 01:18:39 -0500

    adición archivo sass

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -2,14 +2,13 @@
             <meta name="apple-mobile-web-app-capable" content="yes" />
             <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
             <meta name="format-detection" content="telephone=no" />
-            <link rel="shortcut icon" href="favicon.ico" />
+            
             <link rel="stylesheet" type="text/css" href="css/fontello.css" />
             <link rel="stylesheet" type="text/css" href="css/animate.css" />
             <link rel="stylesheet" type="text/css" href="css/idangerous.swiper.css" />
-            <link rel="stylesheet" type="text/css" href="css/style.css" />
             <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
-            <!--boostrap 3.3.6 -->
-            <link href="fontawesome/css/all.css" rel="stylesheet">
+            <link rel="stylesheet" type="text/css" href="css/style.css" />
+            <link rel="stylesheet" type="text/css" href="fontawesome/css/all.css" >
             <!--load all styles -->
 
 
